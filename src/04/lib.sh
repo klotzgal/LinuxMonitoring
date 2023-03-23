271ca8255877128070d86f62ef08b9568c0260f2
Klotz Galtry <klotzgal@un-m5.kzn.21-school.ru>
2023-03-23 04:53:04 +0300

    red

--- src/04/lib.sh
+++ src/04/lib.sh
@@ -26,6 +26,33 @@ color() {
     echo "$color_string"
 }
 
+read_config() {
+    . colors.conf
+	export c1=$column1_background
+    if [ -z $c1 ]; then c1="default"; else mass[0]=$c1; fi
+	export c2=$column1_font_color
+    if [ -z $c2 ]; then c2="default"; else mass[1]=$c2; fi
+	export c3=$column2_background
+    if [ -z $c3 ]; then c3="default"; else mass[2]=$c3; fi
+	export c4=$column2_font_color
+    if [ -z $c4 ]; then c4="default"; else mass[3]=$c4; fi
+}
+
+print_config() {
+    echo ""
+    echo "Column 1 background = ${mass[0]} (${palitra[c1 - 1]})"
+    echo "Column 1 font color = ${mass[1]} (${palitra[c2 - 1]})"
+    echo "Column 2 background = ${mass[2]} (${palitra[c3 - 1]})"
+    echo "Column 2 font color = ${mass[3]} (${palitra[c4 -1 ]})"
+}
+
+default_config() {
+    if [ $1 = "default" ]; then c1=6; mass[0]=$1; fi
+    if [ $2 = "default" ]; then c2=1; mass[1]=$2; fi
+    if [ $3 = "default" ]; then c3=2; mass[2]=$3; fi
+    if [ $4 = "default" ]; then c4=4; mass[3]=$4; fi
+}
+
 info() {
 
 printf "$(color $1 $2)HOSTNAME =${end}$(color $3 $4) $(hostname)${end}\n"
--- src/04/main.sh
+++ src/04/main.sh
@@ -25,35 +25,7 @@ else
 fi
 }
 
-read_config() {
-    . colors.conf
-	export c1=$column1_background
-    if [ -z $c1 ]; then c1="default"; else mass[0]=$c1; fi
-	export c2=$column1_font_color
-    if [ -z $c2 ]; then c2="default"; else mass[1]=$c2; fi
-	export c3=$column2_background
-    if [ -z $c3 ]; then c3="default"; else mass[2]=$c3; fi
-	export c4=$column2_font_color
-    if [ -z $c4 ]; then c4="default"; else mass[3]=$c4; fi
 
-    # for ((i = 0; i < 4; i++)) do
-    #     if mass[$i]
-    # done
-}
-
-print_config() {
-    echo "\nColumn 1 background = ${mass[0]} (${palitra[c1 - 1]})"
-    echo "Column 1 font color = ${mass[1]} (${palitra[c2 - 1]})"
-    echo "Column 2 background = ${mass[2]} (${palitra[c3 - 1]})"
-    echo "Column 2 font color = ${mass[3]} (${palitra[c4 -1 ]})"
-}
-
-default_config() {
-    if [ $1 = "default" ]; then c1=6; mass[0]=$1; fi
-    if [ $2 = "default" ]; then c2=1; mass[1]=$2; fi
-    if [ $3 = "default" ]; then c3=2; mass[2]=$3; fi
-    if [ $4 = "default" ]; then c4=4; mass[3]=$4; fi
-}
 
 main() {
     read_config
