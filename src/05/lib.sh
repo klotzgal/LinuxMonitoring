73f7e6da09cb76a5c4cafbbbcfd0ba84002bbe42
Klotz Galtry <klotzgal@un-m5.kzn.21-school.ru>
2023-03-24 04:01:45 +0300

    закончу завтра

--- src/05/lib.sh
+++ src/05/lib.sh
@@ -13,11 +13,11 @@ fi
 total_folders="$(find $dir -type d | wc | awk '{print $1}')"
 total_files="$(find $dir -type f | wc | awk '{print $1}')"
 
-top_5=$(du -hd 1 $dir | sort -rh | head -n 5 | awk '{print NR" - "$2" "$1}' | column -t)
+
 echo "Total number of folders (including all nested ones) = $total_folders"
 echo "TOP 5 folders of maximum size arranged in descending order (path and size):"
+top_5=$(du -hd 1 ${dir%?} | sort -rh | head -n 5 | awk '{print NR" - "$2"/, "$1}' | column -t)
 echo "$top_5"
-
 
 conf_count="$(find $dir -type f -name "*.conf" | wc | awk '{print $1}')"
 text_count="$(find $dir -type f -name "*.txt" | wc | awk '{print $1}')"
@@ -34,6 +34,14 @@ echo "Executable files = $exe_count"
 echo "Log files (with the extension .log) = $log_count"
 echo "Archive files = $arc_count"
 echo "Symbolic links = $link_count"
+
+echo "TOP 10 files of maximum size arranged in descending order (path, size and type):"
+echo ""
+echo "TOP 10 executable files of the maximum size arranged in descending order (path, size and MD5 hash of file)"
+echo ""
+echo "Script execution time (in seconds) = "
+
+
 
 
 
