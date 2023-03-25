a77f58f7159cde7113d10793d631342c9af8a9ed
Klotz Galtry <klotzgal@un-m5.kzn.21-school.ru>
2023-03-26 00:01:49 +0300

    .

--- src/05/lib.sh
+++ src/05/lib.sh
@@ -18,7 +18,7 @@ fi
 
 
 
-start_time=$(date +"%s.%2N")
+start_time=$(date +"%S.%2N")
 
 total_folders="$(find $dir -type d | wc | awk '{print $1}')"
 total_files="$(find $dir -type f | wc | awk '{print $1}')"
@@ -50,11 +50,17 @@ echo "TOP 10 files of maximum size arranged in descending order (path, size and 
 top_files=$(find ${dir%?} -type f -exec du -h {} \; | sort -rh | awk -F . '{if (NF>1) {print $0" "$NF} else {print $0" - "}}' | awk '{print NR" - "$2", "$3", "$1}' | head -n 10 | column -t)
 echo "$top_files"
 echo "TOP 10 executable files of the maximum size arranged in descending order (path, size and MD5 hash of file)"
+
 top_exe=$(find ${dir%?} -type f -executable | xargs  du -h | sort -rh | head -n 10| awk '{print $2}' | xargs)
+
 count=1
 res="$(for file in $top_exe;do 
     echo "$count - $file, $(du -h $file | awk '{print $1}'), $(md5sum $file | awk '{print $1}')|"
     count=$[$count + 1]
 done)"
+
 echo "$(echo "$res" | column -ts "|" | column -t )"
-echo "Script execution time (in seconds) = $[$start_time - $(date +"%s.%2N")]"
+end_time=$(date +"%S.%2N")
+time=$(echo "$end_time - $start_time" | bc | awk -F . '{if ($1 == "") {$1=0}}; {print $1"."$2}')
+echo "Script execution time (in seconds) = $time"
+echo "Script execution time (in seconds) = $[$start_time - $(date +"%s") | bc]"
